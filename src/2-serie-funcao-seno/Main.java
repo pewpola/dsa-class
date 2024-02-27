af4c1b5b69356c3032af558c1d043932fa1921b6
Eduardo <eduardojor77@gmail.com>
2024-02-26 22:31:16 -0300

    added example

--- src/2-serie-funcao-seno/Main.java
+++ src/2-serie-funcao-seno/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(seno(30, 1));
+        int[] v = new int[]{1, 2, 3, 4, 5};
+        System.out.println(somaBinaria(v));
     }
 
     public static int somaBinaria(int[] v) {
