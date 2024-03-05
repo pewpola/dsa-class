d0400537b4a76689840890d208aa1ff27bbc9544
Eduardo <eduardojor77@gmail.com>
2024-03-04 21:40:56 -0300

    testing

--- src/MontandoArrayList/Main.java
+++ src/MontandoArrayList/Main.java
@@ -3,5 +3,13 @@ package MontandoArrayList;
 import MontandoArrayList.Entidades.StaticList;
 
 public class Main {
-    StaticList list = new StaticList();
+    public static void main(String[] args) {
+        StaticList list = new StaticList();
+
+        list.add(1);
+        list.add(2);
+        list.add(3);
+
+        System.out.println(list);
+    }
 }
