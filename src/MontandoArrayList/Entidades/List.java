e330cdbee3418a73aebfe1168a96c0c27971f8b3
Eduardo <eduardojor77@gmail.com>
2024-03-04 21:17:07 -0300

    mudanças

--- src/MontandoArrayList/Entidades/List.java
+++ src/MontandoArrayList/Entidades/List.java
@@ -3,12 +3,11 @@ import MontandoArrayList.Exceptions.EmptyListException;
 
 public interface List {
     void add(int value);
-    void insert(int value); 
+    void insert(int value);
     void insert(int index, int value) throws IndexOutOfBoundsException;
-    int removeLast() throws EmptyListException;
+    int removeLast() throws EmptyListException ;
     int removeFirst() throws EmptyListException;
-    int removeByIndex(int index) throws IndexOutOfBoundsException, EmptyListException;
-    boolean isFull();
+    int removeByIndex(int index) throws IndexOutOfBoundsException,EmptyListException;
     boolean isEmpty();
     int get(int index) throws IndexOutOfBoundsException;
     void set(int index, int value) throws IndexOutOfBoundsException;
