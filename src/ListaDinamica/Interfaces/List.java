1a5916ae28935b687ac9f9bee609af25965f265e
Eduardo <eduardojor77@gmail.com>
2024-03-07 14:39:02 -0300

    criação de um pacote para interfaces

---
+++ src/ListaDinamica/Interfaces/List.java
@@ -0,0 +1,14 @@
+package ListaDinamica.Interfaces;
+
+public interface List<E> {
+    int size();
+    void add(E value);
+    void insert(E value);
+    void insert(int index, E value) throws IndexOutOfBoundsException;
+    E removeLast() throws EmptyListException;
+    E removeFirst() throws EmptyListException;
+    E removeByIndex(int index) throws IndexOutOfBoundsException,EmptyListException;
+    boolean isEmpty();
+    E get(int index) throws IndexOutOfBoundsException;
+    void set(int index, E value) throws IndexOutOfBoundsException;  
+}
