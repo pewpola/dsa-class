4aea45e90f1ca0e2f9a9b47e6520d9616ffa712c
Eduardo <eduardojor77@gmail.com>
2024-03-06 21:18:39 -0300

    Interface and Exception

--- src/ListaDinamica/Main.java
+++ src/ListaDinamica/Main.java
@@ -5,3 +5,29 @@ public class Main {
         
     }
 }
+
+// Exceptions
+class EmptyListException extends RuntimeException {
+    public EmptyListException(String errorMessage) {
+        super(errorMessage);
+    }
+}
+
+// TAD - List
+
+interface List<E> {
+    int size();
+    void add(E value);
+    void insert(E value);
+    void insert(int index, E value) throws IndexOutOfBoundsException;
+    E removeLast() throws EmptyListException ;
+    E removeFirst() throws EmptyListException;
+    E removeByIndex(int index) throws IndexOutOfBoundsException,EmptyListException;
+    boolean isFull();
+    boolean isEmpty();
+    E get(int index) throws IndexOutOfBoundsException;
+    void set(int index, E value) throws IndexOutOfBoundsException;
+}
+
+
+// LL - LinkedList
