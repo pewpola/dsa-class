8f6f0b4afce56462b6bbe8e5fcee9e80525376f5
Eduardo <eduardojor77@gmail.com>
2024-03-08 14:58:56 -0300

    lista dinamica

---
+++ aulas/listaDinamica/src/app/Main.java
@@ -0,0 +1,10 @@
+package app;
+
+import entities.LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<>(2);
+        System.out.println(list);
+    }
+}
---
+++ aulas/listaDinamica/src/interfaces/List.java
@@ -0,0 +1,16 @@
+package interfaces;
+
+import exceptions.EmptyListException;
+
+public interface List<E> {
+    int size();
+    void add(E value);
+    void insert(E value);
+    void insert(int index, E value) throws IndexOutOfBoundsException;
+    E removeLast() throws EmptyListException;
+    E removeFirst() throws EmptyListException;
+    E removeByIndex(int index) throws IndexOutOfBoundsException, EmptyListException;
+    boolean isEmpty();
+    E get(int index) throws IndexOutOfBoundsException;
+    void set(int index, E value) throws IndexOutOfBoundsException;  
+}
