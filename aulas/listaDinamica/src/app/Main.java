ccbdce0ce2bca2921fd377cab58dd5126bffd501
Eduardo <eduardojor77@gmail.com>
2024-03-11 13:26:03 -0300

    Lista Dinamica

--- aulas/listaDinamica/src/app/Main.java
+++ aulas/listaDinamica/src/app/Main.java
@@ -5,6 +5,7 @@ import entities.LinkedList;
 public class Main {
     public static void main(String[] args) {
         LinkedList<Integer> list = new LinkedList<>(2);
+        list.add(3);
         System.out.println(list);
     }
 }
