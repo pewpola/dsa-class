da6e6946b0c9295d30606593e50441016e8e17ea
Eduardo <eduardojor77@gmail.com>
2024-03-13 21:42:13 -0300

    testing methods on main

--- aulas/listaDinamica/src/app/Main.java
+++ aulas/listaDinamica/src/app/Main.java
@@ -6,6 +6,11 @@ public class Main {
     public static void main(String[] args) {
         LinkedList<Integer> list = new LinkedList<>(2);
         list.add(3);
+        list.add(4);
+        list.add(5);
+        System.out.println(list.get(0));
+        System.out.println(list.get(1));
+        list.set(3, 4);
         System.out.println(list);
     }
 }
