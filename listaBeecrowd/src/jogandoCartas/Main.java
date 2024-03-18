954bef1d83b0b2e52734b4ced1b693eed1445f97
Eduardo <eduardojor77@gmail.com>
2024-03-18 01:21:38 -0300

    solved

--- listaBeecrowd/src/jogandoCartas/Main.java
+++ listaBeecrowd/src/jogandoCartas/Main.java
@@ -1,5 +1,266 @@
 package jogandoCartas;
 
+import java.util.Scanner;
+
 public class Main {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        do {
+            int n = sc.nextInt();
+
+            if (n == 0)
+                break;
+
+            printDiscardedAndRemainingCards(n);
+
+        } while (true);
+
+        sc.close();
+    }
+
+    public static void printDiscardedAndRemainingCards(int n) {
+        String discardedCards = "Discarded cards: ";
+        DoublyLinkedList<Integer> list = new DoublyLinkedList<>();
+
+        for (int i = n; i >= 1; i--) {
+            list.add(i);
+        }
+
+        while (list.size() >= 2) {
+            int removed = list.removeLast();
+            int top = list.removeLast();
+            list.insert(top);
+
+            if (list.size() == 1) {
+                discardedCards += removed;
+            } else {
+                discardedCards += removed + ", ";
+            }
+        }
+
+        System.out.println(discardedCards);
+        System.out.println("Remaining card: " + list.top());
+    }
+}
+
+class DoublyLinkedList<E> {
+
+    private class Node {
+        E value;
+        Node next;
+        Node previous;
+
+        public Node(E value) {
+            this.value = value;
+        }
+    }
+
+    private int size;
+    private Node head;
+    private Node tail;
+
+    public DoublyLinkedList() {
+    }
+
+    public DoublyLinkedList(E value) {
+        add(value);
+    }
+
+    public void add(E value) {
+        Node newNode = new Node(value);
+        if (isEmpty()) {
+            head = newNode;
+        } else {
+            tail.next = newNode;
+            newNode.previous = tail;
+        }
+        tail = newNode;
+        size++;
+    }
+
+    public E get(int index) throws IndexOutOfBoundsException {
+        if (isEmpty()) {
+            throw new EmptyListException("Linked List is Empty");
+        }
+
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException(
+                    "Illegal index " + index + ". Available indexes are [0 - " + (size - 1) + "]");
+        }
+
+        return getNode(index).value;
+    }
+
+    public void insert(E value) {
+        Node newNode = new Node(value);
+
+        if (isEmpty()) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode.next = head;
+            head.previous = newNode;
+            head = newNode;
+        }
+
+        size++;
+    }
+
+    private Node getNode(int index) {
+        Node auxNode = head;
+
+        for (int i = 0; i < index; i++) {
+            auxNode = auxNode.next;
+        }
+
+        return auxNode;
+    }
+
+    public void insert(int index, E value) throws IndexOutOfBoundsException {
+        if (index <= 0) {
+            insert(value);
+        } else if (index >= size) {
+            add(value);
+        } else {
+            Node newNode = new Node(value);
+            Node auxNode1 = getNode(index);
+            Node auxNode2 = getNode(index - 1);
+
+            newNode.next = auxNode2.next;
+            newNode.previous = auxNode1.previous;
+            auxNode2.next = newNode;
+            auxNode1.previous = newNode;
+        }
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public E removeByIndex(int index) throws IndexOutOfBoundsException, EmptyListException {
+        if (isEmpty()) {
+            throw new EmptyListException("Linked List is Empty");
+        }
+
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException(
+                    "Illegal index " + index + ". Available indexes are [0 - " + (size - 1) + "]");
+        }
+
+        E value = null;
+
+        if (index == 0) {
+            value = removeFirst();
+        } else if (index == size - 1) {
+            value = removeLast();
+        } else {
+            Node auxNode1 = getNode(index - 1);
+            Node auxNode2 = auxNode1.next;
+            auxNode1.next = auxNode2.next;
+            auxNode1.next.previous = auxNode1;
+
+            value = auxNode2.value;
+            size--;
+        }
+
+        return value;
+    }
+
+    public E removeFirst() throws EmptyListException {
+        if (isEmpty()) {
+            throw new EmptyListException("Linked List is Empty");
+        }
+
+        Node auxNode = head;
+
+        if (size == 1) {
+            head = null;
+            tail = null;
+        } else {
+            head = head.next;
+            auxNode.next = null;
+        }
+        size--;
+
+        return auxNode.value;
+    }
+
+    public E removeLast() throws EmptyListException {
+        if (isEmpty()) {
+            throw new EmptyListException("Linked List is Empty");
+        }
+
+        E value = tail.value;
+
+        if (size == 1) {
+            head = null;
+            tail = null;
+        } else {
+            Node auxNode = getNode(size - 2);
+            tail = auxNode;
+            tail.next = null;
+        }
+        size--;
+        return value;
+    }
+
+    public void set(int index, E value) throws IndexOutOfBoundsException {
+        if (isEmpty()) {
+            throw new EmptyListException("Linked List is Empty");
+        }
+
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException(
+                    "Illegal index " + index + ". Available indexes are [0 - " + (size - 1) + "]");
+        }
+
+        getNode(index).value = value;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder("[");
+        Node auxNode = head;
+        while (auxNode != null) {
+            sb.append(auxNode.value);
+            if (auxNode.next != null) {
+                sb.append(", ");
+            }
+            auxNode = auxNode.next;
+        }
+        return sb.append("]").toString();
+    }
+
+    public String inverseList() {
+        StringBuilder sb = new StringBuilder("[");
+        Node auxNode = tail;
+        while (auxNode != null) {
+            sb.append(auxNode.value);
+            if (auxNode.previous != null) {
+                sb.append(", ");
+            }
+            auxNode = auxNode.previous;
+        }
+        return sb.append("]").toString();
+    }
+
+    public void clear() {
+        head = null;
+        tail = null;
+        size = 0;
+    }
+
+    public E top() {
+        return tail.value;
+    }
+}
+
+class EmptyListException extends RuntimeException {
+    public EmptyListException(String errorMessage) {
+        super(errorMessage);
+    }
 }
