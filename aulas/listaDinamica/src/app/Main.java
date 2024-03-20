c276d790535288277d5a4be3920eecae110c16b2
Eduardo <eduardojor77@gmail.com>
2024-03-19 21:49:08 -0300

    insertOrdened running

--- aulas/listaDinamica/src/app/Main.java
+++ aulas/listaDinamica/src/app/Main.java
@@ -4,12 +4,18 @@ import entities.LinkedList;
 
 public class Main {
     public static void main(String[] args) {
-        LinkedList<Integer> list = new LinkedList<>(2);
-        list.add(3);
-        list.add(4);
-        list.add(5);
+        LinkedList<Integer> list = new LinkedList<>();
+        list.insertOrdened(3);
         System.out.println(list);
-        list.removeByIndex(2);
+        list.insertOrdened(2);
+        System.out.println(list);
+        list.insertOrdened(5);
+        System.out.println(list);
+        list.insertOrdened(1);
+        System.out.println(list);
+        list.insertOrdened(4);
+        System.out.println(list);
+        list.insertOrdened(0);
         System.out.println(list);
     }
 }
