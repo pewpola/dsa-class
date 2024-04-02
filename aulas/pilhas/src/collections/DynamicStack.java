dbe83868a530a3b78d54cff9e15b497180006140
Eduardo <eduardojor77@gmail.com>
2024-04-01 22:12:00 -0300

    class Node

--- aulas/pilhas/src/collections/DynamicStack.java
+++ aulas/pilhas/src/collections/DynamicStack.java
@@ -1,5 +1,47 @@
 package collections;
 
-public class DynamicStack {
+public class DynamicStack<E> implements Stack<E>{
+    
+    class Node {
+        E value;
+        Node next;
+
+        public Node (E value) {
+            this.value = value;
+        }
+
+        private Node top;
+        private int height;
+    }
+    
+    @Override
+    public int height() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public E pop() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void push(E value) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public E top() {
+        // TODO Auto-generated method stub
+        return null;
+    }
     
 }
