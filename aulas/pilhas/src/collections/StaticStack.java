011f88c2d6e4021bb94089f880908ba8974a0385
Eduardo <eduardojor77@gmail.com>
2024-04-01 21:30:22 -0300

    created StaticStack

---
+++ aulas/pilhas/src/collections/StaticStack.java
@@ -0,0 +1,35 @@
+package collections;
+
+public class StaticStack<E> implements Stack<E>{
+
+    @Override
+    public int height() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public E pop() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void push(E value) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public E top() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+}
