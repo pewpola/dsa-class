eacaa0b49f0d3a5c7bcf8947deb4cd4049cc4150
Eduardo <eduardojor77@gmail.com>
2024-04-03 22:16:54 -0300

    testing dequeue

--- aulas/filas/src/App.java
+++ aulas/filas/src/App.java
@@ -3,13 +3,15 @@ import collections.StaticQueue;
 public class App {
     public static void main(String[] args) throws Exception {
         StaticQueue<Integer> queue = new StaticQueue<>(10);
-        
+
         queue.enqueue(1);
         queue.enqueue(2);
         System.out.println(queue);
         queue.enqueue(3);
         System.out.println(queue);
         queue.enqueue(4);
+        System.out.println(queue);
+        queue.dequeue();
         System.out.println(queue);
     }
 }
