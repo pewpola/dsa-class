ee4347fb63981349c5112012c872eb65246b6b6d
Eduardo <eduardojor77@gmail.com>
2024-04-03 21:48:55 -0300

    testing enqueue

--- aulas/filas/src/App.java
+++ aulas/filas/src/App.java
@@ -1,5 +1,11 @@
+import collections.StaticQueue;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        StaticQueue<Integer> queue = new StaticQueue<>(10);
+        
+        queue.enqueue(1);
+        queue.enqueue(2);
+        queue.enqueue(3);
     }
 }
