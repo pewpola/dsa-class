6ead3bc5ca4d1bb6bfb37f7cb087e3b3d8ba6fcd
Eduardo <eduardojor77@gmail.com>
2024-04-06 10:45:20 -0300

    Node, first, isEmpty, size

--- aulas/filas/src/collections/DynamicQueue.java
+++ aulas/filas/src/collections/DynamicQueue.java
@@ -1,5 +1,45 @@
 package collections;
 
-public class DynamicQueue {
+public class DynamicQueue<E> implements Queue<E> {
+
+    class Node {
+        E value;
+        Node next;
+
+        public Node (E value) {
+            this.value = value;
+        }
+    }
+
+    private Node head;
+    private Node tail;
+    private int size;
+
+    @Override
+    public E dequeue() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void enqueue(E value) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public E first() {
+        return head.value;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
     
 }
