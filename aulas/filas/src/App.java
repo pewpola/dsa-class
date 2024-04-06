064710b557e5da584507e7a0a9c1bf5fe7436ccf
Eduardo <eduardojor77@gmail.com>
2024-04-06 10:56:47 -0300

    testing queue methods

--- aulas/filas/src/App.java
+++ aulas/filas/src/App.java
@@ -1,17 +1,12 @@
-import collections.StaticQueue;
+import collections.DynamicQueue;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        StaticQueue<Integer> queue = new StaticQueue<>(10);
+        DynamicQueue<Integer> queue = new DynamicQueue<>();
 
         queue.enqueue(1);
         queue.enqueue(2);
-        System.out.println(queue);
         queue.enqueue(3);
-        System.out.println(queue);
-        queue.enqueue(4);
-        System.out.println(queue);
-        queue.dequeue();
         System.out.println(queue);
     }
 }
