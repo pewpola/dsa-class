122adaaf70a6d3a218939013b1703ce6ab5585cf
Eduardo <eduardojor77@gmail.com>
2024-04-06 11:35:40 -0300

    testing methods

--- aulas/filas/src/App.java
+++ aulas/filas/src/App.java
@@ -7,6 +7,10 @@ public class App {
         queue.enqueue(1);
         queue.enqueue(2);
         queue.enqueue(3);
+        queue.dequeue();
+        System.out.println(queue);
+        queue.enqueue(4);
+        queue.dequeue();
         System.out.println(queue);
     }
 }
