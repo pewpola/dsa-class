66245f859b446e3c28743ec72dfac0ec4c42e69c
Eduardo <eduardojor77@gmail.com>
2024-04-08 22:07:54 -0300

    created CircularLinkedQueue

---
+++ aulas/filasCirculares/src/collections/CircularLinkedQueue.java
@@ -0,0 +1,35 @@
+package collections;
+
+public class CircularLinkedQueue<E> implements Queue<E> {
+
+    @Override
+    public E dequeue() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void enqueue(E value) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public E first() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public int size() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+       
+}
