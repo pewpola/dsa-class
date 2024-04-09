2f5ba6ccdd22719905507077527acc1e620fad39
Eduardo <eduardojor77@gmail.com>
2024-04-08 22:04:32 -0300

    testing enqueue and dequeue

--- aulas/filasCirculares/src/App.java
+++ aulas/filasCirculares/src/App.java
@@ -1,5 +1,15 @@
+import collections.CircularStaticQueue;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        CircularStaticQueue<Integer> queue = new CircularStaticQueue<>(5);
+
+        queue.enqueue(1);
+        queue.enqueue(2);
+        queue.enqueue(3);
+        queue.enqueue(4);
+        queue.enqueue(5);
+        queue.dequeue();
+        queue.enqueue(6);
     }
 }
