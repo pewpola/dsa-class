7414a4828ca6eb77fb55b56346d358fb69f25f14
Eduardo <eduardojor77@gmail.com>
2024-04-10 21:31:21 -0300

    created LinkedDeque

---
+++ aulas/deques/src/collections/LinkedDeque.java
@@ -0,0 +1,53 @@
+package collections;
+
+public class LinkedDeque<E> implements Deque<E> {
+
+    @Override
+    public void add(E value) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public E first() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void insert(E value) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public boolean isEmpty() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public E last() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public E removeFirst() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public E removeLast() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public int size() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+    
+}
