c5461a3302e206e61a7aaad88990156f7ee50222
Eduardo <eduardojor77@gmail.com>
2024-04-15 21:54:27 -0300

    insert method

--- aulas/filasDePrioridade/src/collections/UnsortedPriorityQueue.java
+++ aulas/filasDePrioridade/src/collections/UnsortedPriorityQueue.java
@@ -1,5 +1,31 @@
 package collections;
 
-public class UnsortedPriorityQueue {
+public class UnsortedPriorityQueue<K,V> extends AbstractPriorityQueue<K,V> {
+
+    @Override
+    public void insert(K key, V value) {
+        Node newNode = new Node(key, value);
+
+        if (isEmpty()) {
+            first = newNode;
+        } else {
+            last.next = newNode;
+            newNode.previous = last;
+        }
+        last = newNode;
+        size++;
+    }
+
+    @Override
+    public Entry<K, V> maxPriority() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Entry<K, V> remove() {
+        // TODO Auto-generated method stub
+        return null;
+    }
     
 }
