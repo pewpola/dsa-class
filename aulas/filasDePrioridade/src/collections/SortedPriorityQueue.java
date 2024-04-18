c4bc6b600a811e8f99859f331093c27c4e787f9f
Eduardo <eduardojor77@gmail.com>
2024-04-17 21:32:16 -0300

    created SortedPriorityQueue

---
+++ aulas/filasDePrioridade/src/collections/SortedPriorityQueue.java
@@ -0,0 +1,22 @@
+package collections;
+
+public class SortedPriorityQueue<K,V> extends AbstractPriorityQueue<K,V> {
+
+    @Override
+    public void insert(K key, V value) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public Entry<K, V> maxPriority() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Entry<K, V> remove() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+}
