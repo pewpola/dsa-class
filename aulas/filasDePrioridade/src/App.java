ceb4854d6ed5bffbe1a8a36f2a25ca660b0d9e8b
Eduardo <eduardojor77@gmail.com>
2024-04-17 21:24:29 -0300

    testing methods

--- aulas/filasDePrioridade/src/App.java
+++ aulas/filasDePrioridade/src/App.java
@@ -1,5 +1,15 @@
+import collections.UnsortedPriorityQueue;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        UnsortedPriorityQueue<Integer, String> queue = new UnsortedPriorityQueue<>();
+        queue.insert(3, "Ian");
+        queue.insert(2, "Leone");
+        queue.insert(0, "Anna");
+        queue.insert(4, "Eduardo");
+        queue.insert(0, "Nathan");
+        queue.insert(3, "Davi");
+        queue.remove();
+        System.out.println(queue.maxPriority());
     }
 }
