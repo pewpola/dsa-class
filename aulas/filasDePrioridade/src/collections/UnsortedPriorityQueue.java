daae0900817e8a2391c85c581b77c2741e316713
Eduardo <eduardojor77@gmail.com>
2024-04-17 21:21:50 -0300

    starting remove method

--- aulas/filasDePrioridade/src/collections/UnsortedPriorityQueue.java
+++ aulas/filasDePrioridade/src/collections/UnsortedPriorityQueue.java
@@ -1,5 +1,7 @@
 package collections;
 
+import javax.management.RuntimeErrorException;
+
 public class UnsortedPriorityQueue<K,V> extends AbstractPriorityQueue<K,V> {
 
     @Override
@@ -42,7 +44,25 @@ public class UnsortedPriorityQueue<K,V> extends AbstractPriorityQueue<K,V> {
 
     @Override
     public Entry<K, V> remove() {
-        // TODO Auto-generated method stub
+        if (isEmpty()) {
+            throw new RuntimeException("Queue is Empty!");
+        }
+        
+        Node maxPriorityNode = findMaxPriorityNode();
+
+        if (size == 1) {
+            first = null;
+            last = null;    
+        } else {
+            if (maxPriorityNode == first) {
+                System.out.println("Removendo o primeiro...");
+            } else if (maxPriorityNode == last) {
+                System.out.println("Removendo o último...");
+            } else {
+                System.out.println("Removendo sei lá quem...");
+            }
+        }
+
         return null;
     }
     
