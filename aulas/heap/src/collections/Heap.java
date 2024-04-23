1082f09c76ff22e2302de0a5a40fe3a28f96c90d
Eduardo <eduardojor77@gmail.com>
2024-04-22 22:16:02 -0300

    created Heap

---
+++ aulas/heap/src/collections/Heap.java
@@ -0,0 +1,23 @@
+package collections;
+
+public class Heap<K,V> extends AbstractHeap<K,V> {
+
+    @Override
+    public void insert(K key, V value) {
+        // TODO Auto-generated method stub
+        super.insert(key, value);
+    }
+
+    @Override
+    public Entry<K, V> maxPriority() {
+        // TODO Auto-generated method stub
+        return super.maxPriority();
+    }
+
+    @Override
+    public Entry<K, V> remove() {
+        // TODO Auto-generated method stub
+        return super.remove();
+    }
+    
+}
