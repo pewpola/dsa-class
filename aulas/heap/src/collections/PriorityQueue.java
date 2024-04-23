79db9ffb65ca7eaf35811b3083595bad8ee64356
Eduardo <eduardojor77@gmail.com>
2024-04-22 21:55:56 -0300

    created PriorityQueue interface

---
+++ aulas/heap/src/collections/PriorityQueue.java
@@ -0,0 +1,9 @@
+package collections;
+
+public interface PriorityQueue<K,V> {
+    void insert(K key, V value);
+    Entry<K,V> maxPriority();
+    Entry<K,V> remove();
+    int size();
+    boolean isEmpty();
+}
