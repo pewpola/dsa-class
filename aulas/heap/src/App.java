28f88caf38c77cac53c04f50c8c67800c479971f
Eduardo <eduardojor77@gmail.com>
2024-04-24 17:53:38 -0300

    testing insert method

--- aulas/heap/src/App.java
+++ aulas/heap/src/App.java
@@ -1,5 +1,17 @@
+import collections.*;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        PriorityQueue<Integer, String> heap = new Heap<>();
+        heap.insert(5, "Leone");
+        heap.insert(8, "Jardiana");
+        heap.insert(6, "Anna");
+        heap.insert(12, "Nathan");
+        heap.insert(33, "Tales");
+        heap.insert(29, "Ian LW");
+        heap.insert(39, "Lucas");
+        heap.insert(1, "Davi");
+
+        System.out.println(heap);
     }
 }
