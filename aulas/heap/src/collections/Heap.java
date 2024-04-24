ad2540354de6b994cec5e9110365b6646edd5bee
Eduardo <eduardojor77@gmail.com>
2024-04-23 21:49:20 -0300

    swap method

--- aulas/heap/src/collections/Heap.java
+++ aulas/heap/src/collections/Heap.java
@@ -7,17 +7,23 @@ public class Heap<K,V> extends AbstractHeap<K,V> {
         
     }
 
+    private void swap(int index1, int index2) {
+        Entry<K,V> auxEntry = heap.get(index1);
+        heap.set(index1, heap.get(index2));
+        heap.set(index2, auxEntry);
+    }
+    
+    private int parent(int child) {
+        return (child - 1) / 2;
+    }
+    
     @Override
     public Entry<K, V> maxPriority() {
         if (isEmpty()) {
             throw new RuntimeException("Heap is Empty!");
         }
-
+    
         return heap.get(0);
-    }
-
-    private int parent(int child) {
-        return (child - 1) / 2;
     }
 
     @Override
