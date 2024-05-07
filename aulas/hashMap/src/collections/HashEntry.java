a04487ea9b6146f6433eed409a7b2e0e48796614
Eduardo <eduardojor77@gmail.com>
2024-05-06 21:17:22 -0300

    adding constructor for hash entry

--- aulas/hashMap/src/collections/HashEntry.java
+++ aulas/hashMap/src/collections/HashEntry.java
@@ -5,9 +5,18 @@ public class HashEntry<K,V> implements Entry<K,V> {
     private K key;
     private V value;
 
+    public hashEntry(K key, V Value) {
+        this.key = key;
+        this.value = value;
+    }
+
     @Override
     public K getKey() {
         return key;
+    }
+
+    public void setKey(K key) {
+        this.key = key;
     }
 
     @Override
