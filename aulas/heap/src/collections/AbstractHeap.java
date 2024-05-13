14d88e1f2159f710dcb88462f855c6fffe3895c2
pewpola <eduardojor77@gmail.com>
2024-05-13 19:28:55 -0300

    adding methods

--- aulas/heap/src/collections/AbstractHeap.java
+++ aulas/heap/src/collections/AbstractHeap.java
@@ -6,12 +6,12 @@ import entities.ArrayList;
 import entities.DefaultComparator;
 import entities.List;
 
-public abstract class AbstractHeap<K,V> implements PriorityQueue<K,V> {
+public abstract class AbstractHeap<K, V> implements PriorityQueue<K, V> {
 
-    class HeapEntry implements Entry<K,V> {
+    class HeapEntry implements Entry<K, V> {
 
         K key;
-        V value;        
+        V value;
 
         public HeapEntry(K key, V value) {
             this.key = key;
@@ -29,12 +29,21 @@ public abstract class AbstractHeap<K,V> implements PriorityQueue<K,V> {
         }
 
         @Override
+        public void setValue(V newValue) {
+            this.value = newValue;
+        }
+
+        public void setKey(K newKey) {
+            this.key = newKey;
+        }
+
+        @Override
         public String toString() {
             return "(" + key + ":" + value + ")";
         }
     }
 
-    protected List<Entry<K,V>> heap;
+    protected List<Entry<K, V>> heap;
     private Comparator<K> comparator;
 
     public AbstractHeap() {
@@ -42,11 +51,11 @@ public abstract class AbstractHeap<K,V> implements PriorityQueue<K,V> {
         comparator = new DefaultComparator<>();
     }
 
-    public int compare(int index1, int index2){
+    public int compare(int index1, int index2) {
         return compare(heap.get(index1), heap.get(index2));
     }
 
-    public int compare(Entry<K,V> e1, Entry<K,V> e2){
+    public int compare(Entry<K, V> e1, Entry<K, V> e2) {
         return comparator.compare(e1.getKey(), e2.getKey());
     }
 
@@ -56,12 +65,12 @@ public abstract class AbstractHeap<K,V> implements PriorityQueue<K,V> {
     }
 
     @Override
-    public int size() {        
+    public int size() {
         return heap.size();
     }
 
     @Override
     public String toString() {
         return heap.toString();
-    }    
+    }
 }
