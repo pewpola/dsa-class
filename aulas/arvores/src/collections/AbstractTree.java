7a596073e90b98a4552910d9cf3e7148da41b42a
pewpola <eduardojor77@gmail.com>
2024-05-20 22:06:18 -0300

    developing AbstractTree

--- aulas/arvores/src/collections/AbstractTree.java
+++ aulas/arvores/src/collections/AbstractTree.java
@@ -1,9 +1,59 @@
 package collections;
 
+import java.util.*;
+
 public class AbstractTree<E> implements Tree<E> {
 
     class Node {
         E value;
-        
+        Node left;
+        Node right;
+
+        public Node(E value) {
+            this.value = value;
+        }
     }
+
+    protected Node root;
+    protected int size;
+    protected Comparator<E> comparator;
+
+    @Override
+    public boolean contains(E value) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public E delete(E value) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void insert(E value) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public boolean isEmpty() {
+        if (root == null) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public int size() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public void treeTraversal() {
+        // TODO Auto-generated method stub
+
+    }
+
 }
