4faf191b3a1ede77730136682516890f5648c805
pewpola <eduardojor77@gmail.com>
2024-06-03 21:32:59 -0300

    testing height method

--- aulas/arvores/src/App.java
+++ aulas/arvores/src/App.java
@@ -1,14 +1,27 @@
-import collections.BinarySearchTree;
+import collections.AVLTree;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        BinarySearchTree<Integer> bst = new BinarySearchTree<>();
+        AVLTree<Integer> avl = new AVLTree<>();
         
-        bst.insert(50);
-        bst.insert(1442);
-        bst.insert(21);
-        bst.insert(80);
-        System.out.println(bst.contains(80));
-        System.out.println(bst);
+        // bst.insert(60);
+        // bst.insert(30);
+        // bst.insert(90);
+        // bst.insert(5);
+        // bst.insert(45);
+        // bst.insert(85);
+        // bst.insert(150);
+        // bst.insert(43);
+        // bst.insert(70);
+        // bst.insert(87);
+        // bst.insert(100);
+        // bst.insert(200);
+
+        avl.insert(5);
+        avl.insert(1);
+        avl.insert(1);
+        avl.insert(100);
+
+        System.out.println(avl.height());
     }
 }
