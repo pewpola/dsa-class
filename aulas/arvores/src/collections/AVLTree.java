e21973f2f90cd9673a27bdd19096f283841aba4c
pewpola <eduardojor77@gmail.com>
2024-06-05 17:44:45 -0300

    implementing insert and rotation methods

--- aulas/arvores/src/collections/AVLTree.java
+++ aulas/arvores/src/collections/AVLTree.java
@@ -34,6 +34,24 @@ public class AVLTree<E> extends AbstractTree<E> {
         return super.delete(value);
     }
 
+    private Node rightRotation(Node x) {
+        Node y = x.left;
+        Node T2 = y.right;
+        y.right = x;
+        x.left = T2;
+
+        return y;
+    }
+
+    private Node leftRotation(Node x) {
+        Node y = x.right;
+        Node T2 = y.left;
+        y.left = x;
+        x.right = T2;
+        
+        return y;
+    }
+
     private Node insert(E value, Node current) {
         if (current == null) {
             size++;
@@ -46,12 +64,56 @@ public class AVLTree<E> extends AbstractTree<E> {
             current.right = insert(value, current.right);
         }
         
+        int diff1 = height(current.right) - height(current.left);
+
+        if (diff1 > 1) {
+            System.out.println("Desbalanceada - Direita");
+            int diff2 = height(current.right.right) - height(current.right.left);
+
+            if (diff2 == 0 || diff2 == 1) {
+                System.out.println("Rotação esquerda!");
+                current = leftRotation(current);
+            } else {
+                System.out.println("Rotação direita do filho e rotação esquerda!");
+                current.right = rightRotation(current.right);
+                current = leftRotation(current);
+            }
+        } else if (diff1 < -1) {
+            System.out.println("Desbalanceada - Esquerda");
+            int diff2 = height(current.right.right) - height(current.right.left);
+
+            if (diff2 == 0 || diff2 == -1) {
+                System.out.println("Rotação direita!");
+                current = rightRotation(current);
+            } else {
+                System.out.println("Rotação esquerda do filho e rotação direita!");
+                current.left = leftRotation(current.left);
+                current = rightRotation(current);
+            }
+
+        }
+
         return current;
     }
     
     @Override
     public void insert(E value) {
         root = insert(value, root);
+    }
+
+    private StringBuilder preOrder(StringBuilder sb, Node current) {
+        if (current != null) {
+            sb.append(current.value + " ");
+            preOrder(sb, current.left);
+            preOrder(sb, current.right);
+        }
+
+        return sb;
+    }
+
+    @Override
+    public String toString() {
+        return "";
     }
     
 }
