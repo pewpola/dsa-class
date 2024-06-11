b82565b61da6adec195765c4fc9217f740a6ce58
pewpola <eduardojor77@gmail.com>
2024-06-10 21:15:41 -0300

    insert method

--- aulas/arvores/src/collections/AVLTree.java
+++ aulas/arvores/src/collections/AVLTree.java
@@ -9,29 +9,37 @@ public class AVLTree<E> extends AbstractTree<E> {
             return b;
         }
     }
-
+    
+    private void updateHeight(Node current){
+        current.height = 1+max(height(current.left), height(current.right));
+    }
     private int height(Node current) {
         if (current == null) {
             return 0;
         }
-
-        return 1 + max(height(current.left), height(current.right));
+        return current.height;
     }
 
-    public int height () {
+    public int height() {
         return height(root);
     }
-    
+
     @Override
     public boolean contains(E value) {
         // TODO Auto-generated method stub
-        return super.contains(value);
+        return false;
     }
-
+    
     @Override
     public E delete(E value) {
         // TODO Auto-generated method stub
-        return super.delete(value);
+        return null;
+    }
+
+
+    @Override
+    public void insert(E value) {
+        root = insert(value, root);
     }
 
     private Node rightRotation(Node x) {
@@ -39,81 +47,82 @@ public class AVLTree<E> extends AbstractTree<E> {
         Node T2 = y.right;
         y.right = x;
         x.left = T2;
-
+        updateHeight(x);
+        updateHeight(y);
         return y;
     }
-
+    
     private Node leftRotation(Node x) {
         Node y = x.right;
         Node T2 = y.left;
         y.left = x;
         x.right = T2;
-        
+        updateHeight(x);
+        updateHeight(y);
         return y;
     }
+
+    private Node balance(Node current){
+        int difference1 = height(current.right) - height(current.left);
+        if (difference1 > 1) {
+            System.out.println("Desbalanceado - Direita");
+            int difference2 = height(current.right.right) - height(current.right.left);
+            if (difference2 == 0 || difference2 == 1) {
+                current = leftRotation(current);
+            } else {
+                current.right = rightRotation(current.right);
+                current = leftRotation(current);
+            }
+
+        } else if (difference1 < -1) {
+            System.out.println("Desbalanceado - Esquerda");
+            int difference2 = height(current.left.right) - height(current.left.left);
+            if (difference2 == 0 || difference2 == -1) {
+                current = rightRotation(current);
+            } else {
+                current.left = leftRotation(current.left);
+                current = rightRotation(current);
+            }
+        }
+        return current;
+    }
+
 
     private Node insert(E value, Node current) {
         if (current == null) {
             size++;
             return new Node(value);
         }
-        
         if (compare(value, current) < 0) {
             current.left = insert(value, current.left);
         } else if (compare(value, current) > 0) {
             current.right = insert(value, current.right);
         }
-        
-        int diff1 = height(current.right) - height(current.left);
-
-        if (diff1 > 1) {
-            System.out.println("Desbalanceada - Direita");
-            int diff2 = height(current.right.right) - height(current.right.left);
-
-            if (diff2 == 0 || diff2 == 1) {
-                System.out.println("Rotação esquerda!");
-                current = leftRotation(current);
-            } else {
-                System.out.println("Rotação direita do filho e rotação esquerda!");
-                current.right = rightRotation(current.right);
-                current = leftRotation(current);
-            }
-        } else if (diff1 < -1) {
-            System.out.println("Desbalanceada - Esquerda");
-            int diff2 = height(current.right.right) - height(current.right.left);
-
-            if (diff2 == 0 || diff2 == -1) {
-                System.out.println("Rotação direita!");
-                current = rightRotation(current);
-            } else {
-                System.out.println("Rotação esquerda do filho e rotação direita!");
-                current.left = leftRotation(current.left);
-                current = rightRotation(current);
-            }
-
-        }
-
+        //atualiza a altura
+        updateHeight(current);
+        current = balance(current);        
         return current;
     }
-    
+
     @Override
-    public void insert(E value) {
-        root = insert(value, root);
+    public void treeTraversal() {
+        // TODO Auto-generated method stub
+
     }
 
     private StringBuilder preOrder(StringBuilder sb, Node current) {
         if (current != null) {
-            sb.append(current.value + " ");
+            sb.append(current).append("\n");
             preOrder(sb, current.left);
             preOrder(sb, current.right);
-        }
 
+        }
         return sb;
     }
 
     @Override
     public String toString() {
-        return "";
+        return preOrder(new StringBuilder(), root).toString();
     }
-    
+
 }
