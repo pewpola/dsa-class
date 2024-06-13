2bde172fa3a037fe2cc81e4dd8467e3349ee85ca
pewpola <eduardojor77@gmail.com>
2024-06-13 10:55:40 -0300

    fixing delete method

--- aulas/arvores/src/collections/RecursiveBinarySearchTree.java
+++ aulas/arvores/src/collections/RecursiveBinarySearchTree.java
@@ -7,15 +7,14 @@ public class RecursiveBinarySearchTree<E> extends AbstractTree<E> {
             return null;
         }
 
-        if (compare(value, current) == 0) {
+        if (compare(value, current) < 0) {
             current = contains(value, current.left);
-        } else if (compare(value, current) < 0) {
+        } else if (compare(value, current) > 0) {
             current = contains(value, current.right);
         }
-
         return current;
     }
-
+    
     @Override
     public boolean contains(E value) {
         return contains(value, root) != null;
@@ -25,9 +24,9 @@ public class RecursiveBinarySearchTree<E> extends AbstractTree<E> {
         if (current.left == null) {
             return current;
         }
-
         return minNode(current.left);
     }
+
 
     private Node delete(E value, Node current) {
         if (current == null) {
@@ -37,7 +36,7 @@ public class RecursiveBinarySearchTree<E> extends AbstractTree<E> {
         if (compare(value, current) < 0) {
             current.left = delete(value, current.left);
         } else if (compare(value, current) > 0) {
-            current.left = delete(value, current.right);
+            current.right = delete(value, current.right);
         } else {
             if (current.left == null && current.right == null) {
                 size--;
@@ -45,12 +44,12 @@ public class RecursiveBinarySearchTree<E> extends AbstractTree<E> {
             } else if (current.left != null && current.right != null) {
                 current.value = minNode(current.right).value;
                 current.right = delete(current.value, current.right);
+
             } else {
                 size--;
                 current = current.left != null ? current.left : current.right;
             }
         }
-
         return current;
     }
 
@@ -58,11 +57,9 @@ public class RecursiveBinarySearchTree<E> extends AbstractTree<E> {
     public E delete(E value) {
         int previousSize = size;
         root = delete(value, root);
-        
         if (previousSize != size) {
             return value;
         }
-        
         return null;
     }
 
@@ -71,32 +68,20 @@ public class RecursiveBinarySearchTree<E> extends AbstractTree<E> {
             size++;
             return new Node(value);
         }
-        
         if (compare(value, current) < 0) {
             current.left = insert(value, current.left);
         } else if (compare(value, current) > 0) {
             current.right = insert(value, current.right);
         }
-        
         return current;
-    }
-    
-    @Override
-    public void insert(E value) {
-        root = insert(value, root);
-    }
-
-    @Override
-    public String toString() {
-        // TODO Auto-generated method stub
-        return super.toString();
     }
 
     private void preOrder(Node current) {
         if (current != null) {
-            System.out.println(current.value + " ");
+            System.out.print(current.value + " ");
             preOrder(current.left);
             preOrder(current.right);
+
         }
     }
 
@@ -104,31 +89,42 @@ public class RecursiveBinarySearchTree<E> extends AbstractTree<E> {
         if (current != null) {
             postOrder(current.left);
             postOrder(current.right);
-            System.out.println(current.value + " ");
+            System.out.print(current.value + " ");
+
         }
     }
 
     private void inOrder(Node current) {
         if (current != null) {
-            inOrder(current.left);
-            System.out.println(current.value + " ");
             inOrder(current.right);
+            System.out.print(current.value + " ");
+            inOrder(current.left);
         }
     }
 
-    @Override
-    public void treeTraversal() {
-        preOrder(root);
-    }
-
     public void treeTraversal(String type) {
-        if (type.equals("postOrder")) {
-            postOrder(root);
-        } else if (type.equals("inOrder")) {
+        if (type.equals("inOrder")) {
             inOrder(root);
+        } else if (type.equals("postOrder")) {
+            postOrder(root);
         } else {
             treeTraversal();
         }
     }
 
+    @Override
+    public void insert(E value) {
+        root = insert(value, root);
+    }
+
+    @Override
+    public void treeTraversal() {
+        preOrder(root);
+
+    }
+
+    @Override
+    public String toString() {
+        return "RecursiveBinarySearchTree []";
+    }
 }
