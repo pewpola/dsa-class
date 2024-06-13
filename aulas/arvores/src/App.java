419e2e02781f3eaf448650f49ccc60ca5fcdce93
pewpola <eduardojor77@gmail.com>
2024-06-13 10:56:02 -0300

    testing insert, delete and treeTraversal methods

--- aulas/arvores/src/App.java
+++ aulas/arvores/src/App.java
@@ -1,26 +1,24 @@
-import collections.AVLTree;
+// import collections.AVLTree;
+import collections.RecursiveBinarySearchTree;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        AVLTree<Integer> avl = new AVLTree<>();
-
-        avl.insert(37);
-        avl.insert(25);
-        System.out.println(avl);
-        System.out.println(avl.height());
-        System.out.println(avl.contains(37));
-        System.out.println(avl.contains(25));
-        // bst.insert(60);
-        // bst.insert(30);
-        // bst.insert(90);
-        // bst.insert(5);
-        // bst.insert(45);
-        // bst.insert(85);
-        // bst.insert(150);
-        // bst.insert(43);
-        // bst.insert(70);
-        // bst.insert(87);
-        // bst.insert(100);
-        // bst.insert(200);
-    }
+        // AVLTree<Integer> avl = new AVLTree<>();
+        RecursiveBinarySearchTree<Integer> bst = new RecursiveBinarySearchTree<>();
+        
+        bst.insert(44);
+        bst.insert(25);
+        bst.insert(22);
+        bst.insert(89);
+        bst.insert(33);
+        bst.insert(98);
+        bst.insert(29);
+        bst.treeTraversal();
+        bst.delete(33);
+        bst.treeTraversal();
+        // System.out.println(avl);
+        // System.out.println(avl.height());
+        // System.out.println(avl.contains(37));
+        // System.out.println(avl.contains(25));
+        }
 }
